81d0fbd7cbe1b7c0b4e3bc659bab62e7319e45bb
Ritoja <127992034+RitojaBanerjee@users.noreply.github.com>
2024-12-20 17:21:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from turtle import Screen
-import time
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
+import time
 
 #Screen of the game
 screen = Screen()
@@ -28,26 +28,38 @@ screen.onkey(key="Left", fun=snake.left)
 screen.onkey(key="Right", fun=snake.right)
 
 #game started - snake is moving
+game_speed = 0.1
 game_is_on = True
 while game_is_on:
-    screen.update()
     time.sleep(0.5)
+    screen.update()
     snake.move()
 
     if snake.head.distance(food) < 15:
+        if food.get_color() == "red":
+            scoreboard.red_score()
+        else:
+            scoreboard.blue_score()
         food.refresh()
         snake.extend()
-        scoreboard.increase_scoreboard()
 
-    if (snake.head.xcor() > 290 or snake.head.ycor() > 290 or
-            snake.head.xcor() < -290 or snake.head.ycor() < -290):
+        if scoreboard.score % 20 == 0 and scoreboard.score > 0:
+            game_speed *= 0.9
+
+    if snake.head.xcor() > 300 :
+        snake.head.goto(-300, snake.head.ycor())
+    elif snake.head.xcor() < -300:
+        snake.head.goto(300, snake.head.ycor())
+
+    if snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
 
+
     for any_segment in snake.segments[1:]:
        if snake.head.distance(any_segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+           game_is_on = False
+           scoreboard.game_over()
 
 
 screen.exitonclick()
